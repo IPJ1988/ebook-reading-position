bdbc486f140c705f9e005f50718e5b04d6c5b27b
hytexts-Chanathip <chanathip@hytexts.com>
2020-01-12 17:39:49 +0700

    Initialize

---
+++ app/src/main/java/com/hytexts/sample/readingposition/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hytexts.sample.readingposition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
