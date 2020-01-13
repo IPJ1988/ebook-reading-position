9791b41e3ab7dd1c0975cec6778646f963c54d0f
hytexts-Chanathip <chanathip@hytexts.com>
2020-01-13 10:41:25 +0700

    Create reading position module

--- app/src/main/java/com/hytexts/sample/readingposition/MainActivity.kt
+++ app/src/main/java/com/hytexts/sample/readingposition/MainActivity.kt
@@ -2,11 +2,14 @@ package com.hytexts.sample.readingposition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hytexts.readingposition.Sample
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Sample().sayHi()
     }
 }
---
+++ reading-position/src/main/java/com/hytexts/readingposition/Sample.kt
@@ -0,0 +1,10 @@
+package com.hytexts.readingposition
+
+import android.util.Log
+
+class Sample {
+
+    fun sayHi() {
+        Log.e("tt", "Hello, ${BuildConfig.LIBRARY_PACKAGE_NAME}")
+    }
+}
